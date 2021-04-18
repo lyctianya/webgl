095ab31ee5ca4a667cf38d0d39386839f85f2572
lyctianya <lyctianya@163.com>
2021-04-18 18:00:12 +0800

    feat: 忘了config文件

---
+++ config/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  dev: {
+    assetsPublicPath: "/", // 相对文件路径
+    proxyTable: {},
+    host: "localhost",
+    port: "3000",
+    autoOpenBrowser: true, // 是否自动打开浏览器
+    devtool: "eval-source-map", // Source Maps
+  },
+};
